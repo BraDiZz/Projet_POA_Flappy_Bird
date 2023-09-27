14dcee02e952958dca0333482a2a97abe784899c
BraDiZz <82169661+BraDiZz@users.noreply.github.com>
2023-09-27 15:07:10 +0200

    Add files via upload
    lol

--- test.py
+++ test.py
@@ -4,92 +4,42 @@ import random
 
 pygame.init()
 
-# Constants
-WIDTH, HEIGHT = 400, 600
+# constantes des tuyaux
+WIDTH, HEIGHT = 600, 800
 WHITE = (255, 255, 255)
 
-# Create the game window
+# Création de la fenêtre
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Flappy Bird Clone")
+pygame.display.set_caption("Floppy cat")
 
-clock = pygame.time.Clock()
 
-class Bird:
-    def __init__(self):
-        self.x = 100
-        self.y = HEIGHT // 2
-        self.velocity = 0
-        self.gravity = 0.5
 
-    def flap(self):
-        self.velocity = -10
-
-    def update(self):
-        self.velocity += self.gravity
-        self.y += self.velocity
-
-    def draw(self):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, int(self.y)), 20)
 
 class Pipe:
     def __init__(self, x):
         self.x = x
         self.height = 0
-        self.gap = 200
-        self.top_height = random.randint(50, 300)
-        self.bottom_height = HEIGHT - self.top_height - self.gap
+        self.width = 20 #épaisseur du tuyau
+        self.gap = 150 #écart entre tuyau du haut et du bas
+        self.top_height = random.randint(50, HEIGHT-HEIGHT/4) #calcule la hauteur minimum des tuyaux en fonction de la taille de la fenêtre
+        self.bottom_height = HEIGHT - self.top_height - self.gap #calcule la hauteur du tuyau du bas
 
-    def update(self):
-        self.x -= 2
+    def draw(self): #dessine la partie haute et basse du tuyau
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, 0, self.width, self.top_height))
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, HEIGHT - self.bottom_height, self.width, self.bottom_height))
 
-    def collides_with_bird(self, bird):
-        if bird.x + 20 > self.x and bird.x - 20 < self.x + 50:
-            if bird.y - 20 < self.top_height or bird.y + 20 > HEIGHT - self.bottom_height:
-                return True
-        return False
+pipes = [Pipe(100),Pipe(300),Pipe(500),Pipe(700)]
 
-    def draw(self):
-        pygame.draw.rect(screen, (0, 255, 0), (self.x, 0, 50, self.top_height))
-        pygame.draw.rect(screen, (0, 255, 0), (self.x, HEIGHT - self.bottom_height, 50, self.bottom_height))
-
-bird = Bird()
-pipes = [Pipe(WIDTH + i * 200) for i in range(4)]
-
-while True:
+while True: #gère la partie
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird.flap()
 
-    bird.update()
-
-    # Check for collisions
-    for pipe in pipes:
-        if pipe.collides_with_bird(bird):
-            pygame.quit()
-            sys.exit()
-
-    # Remove off-screen pipes
-    if pipes[0].x < -50:
-        pipes.pop(0)
-        pipes.append(Pipe(pipes[-1].x + 200))
-
-    # Generate new pipes
-    if pipes[-1].x < WIDTH - 200:
-        pipes.append(Pipe(WIDTH))
-
-    # Clear the screen
     screen.fill(WHITE)
 
-    # Draw objects
+    # dessiner les tuyaux
     for pipe in pipes:
-        pipe.update()
         pipe.draw()
 
-    bird.draw()
-
     pygame.display.update()
-    clock.tick(60)
